65f735f5caa297156918a76f11ed4d7e27873aea
Jiang Siao-Huei <shjiang10@jiangteki-macbook-air.local>
2018-04-24 20:11:40 +0800

    Initial Commit

---
+++ Demo-0424-變色/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo-0424-變色
+//
+//  Created by Jiang Siao-Huei on 2018/4/24.
+//  Copyright © 2018年 Jiang Siao-Huei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
