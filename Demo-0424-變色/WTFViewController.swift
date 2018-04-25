1d5b83b46a61789f53b437dbcf75ab39d2c62c71
Jiang Siao-Huei <shjiang10@jiangteki-macbook-air.local>
2018-04-26 02:12:31 +0800

    變臉大師-花惹發(完成版)

--- Demo-0424-變色/WTFViewController.swift
+++ Demo-0424-變色/WTFViewController.swift
@@ -12,20 +12,21 @@ class WTFViewController: UIViewController {
     
     
     @IBAction func colorViewChange(_ sender: UISlider) {
-        imageView.backgroundColor=UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha:1)
-        if sender==redSlider{
-            redLabel.text="\(redSlider.value)"
-        }else if sender==blueSlider{blueLabel.text="\(blueSlider.value)"
-        }else if sender==greenSlider{greenLabel.text="\(greenSlider.value)"
-        }else if sender==alphaSlider{alphaLabel.text="\(alphaSlider.value)"
-        }
+        imageView.backgroundColor=UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha:CGFloat(alphaSlider.value))
         
-        var number = 2.3456
-        var numberString=String(format: "%.2f", number)
-        redLabel.text=numberString
-        blueLabel.text=numberString
-        greenLabel.text=numberString
-        alphaLabel.text=numberString
+        
+        if sender==redSlider{
+            let numberString=String(format: "%.2f", redSlider.value)
+            redLabel.text=numberString
+        }else if sender==blueSlider{
+            let numberString=String(format: "%.2f", blueSlider.value)
+            blueLabel.text=numberString
+        }else if sender==greenSlider{
+            let numberString=String(format: "%.2f", greenSlider.value)
+            greenLabel.text=numberString
+        }else if sender==alphaSlider{
+            let numberString=String(format: "%.2f", alphaSlider.value)
+            alphaLabel.text=numberString}
     }
     
     
